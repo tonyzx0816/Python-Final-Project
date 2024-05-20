96cb4a7949f37f0d6d30fa581813f7b63ad9e1f7
tonyzx0816 <lizhongxian23@gmail.com>
2024-05-19 18:06:34 -0700

    lasest

--- exercise36.py
+++
@@ -1,8 +0,0 @@
-user_letter = input()
-vowel = ['a','e','i','o','u']
-if user_letter in vowel:
-    print(f'{user_letter} is a vowel.')
-elif user_letter == 'y':
-    print('sometimes y is a vowel, and sometimes y is a consonant.')
-else:
-    print(f'{user_letter} is a consonant.')
